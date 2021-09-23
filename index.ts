d4cd09980429ed45976e9c094c0f0a006baff81f
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-23 12:01:37 +0100

    changes

--- index.ts
+++ index.ts
@@ -12,20 +12,25 @@ client.login(process.env.TOKEN);
 
 client.on("ready", () => {
   console.log("Logged in");
+
+  client.user?.setPresence({
+    status: "online",
+    activity: {
+      name: "by Lakee",
+      type: "CUSTOM_STATUS",
+    },
+  });
 });
 
-app.get(
-  `/${process.env.SECRET}/:message`,
-  async (req: Request, res: Response) => {
-    console.log(req.params.message);
-    const channel: TextChannel | undefined = client.channels.cache.find(
-      (channel) => channel.id === (process.env.CHANNEL_ID as string)
-    ) as TextChannel | undefined;
+app.get(`/`, async (req: Request, res: Response) => {
+  console.log(req.params.message);
+  const channel: TextChannel | undefined = client.channels.cache.find(
+    (channel) => channel.id === (process.env.CHANNEL_ID as string)
+  ) as TextChannel | undefined;
 
-    await channel?.send(req.params.message);
-    res.status(200);
-    res.send("sucess");
-  }
-);
+  await channel?.send(req.params.message);
+  res.status(200);
+  res.send("sucess");
+});
 
 app.listen(5000, () => console.log(`App listening on port 5000`));
