5d10cd1fbc590480da183b7a20a006a0933d4133
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-23 12:21:16 +0100

    moved over to post req

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 config();
 import Discord, { Channel, TextChannel } from "discord.js";
+import { JSONBody } from "./types";
+import { messageText } from "./utils";
 
 const app = express();
 app.use(cors());
@@ -23,26 +25,16 @@ client.on("ready", () => {
   });
 });
 
-app.get(`/`, async (req: Request, res: Response) => {
-  console.log(req.params.message);
-  const channel: TextChannel | undefined = client.channels.cache.find(
-    (channel) => channel.id === (process.env.CHANNEL_ID as string)
-  ) as TextChannel | undefined;
-
-  await channel?.send(req.params.message);
-  res.status(200);
-  res.send("sucess");
-});
-
 app.post(`/`, async (req: Request, res: Response) => {
-  console.log(req.params.message);
+  const body = req.body as JSONBody;
+  console.log(body);
   const channel: TextChannel | undefined = client.channels.cache.find(
     (channel) => channel.id === (process.env.CHANNEL_ID as string)
   ) as TextChannel | undefined;
 
-  await channel?.send(req.params.message);
+  await channel?.send(messageText(body));
   res.status(200);
   res.send("sucess");
 });
 
-app.listen(5000, () => console.log(`App listening on port 5000`));
+app.listen(6969, () => console.log(`App listening on port 6969`));
