f1bce4a13288df58f3e26fd24fa435ddcd95ae6f
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-23 11:31:48 +0100

    added a secret thing

--- index.ts
+++ index.ts
@@ -12,14 +12,17 @@ client.on("ready", () => {
   console.log("Logged in");
 });
 
-app.get("/:message", async (req: Request, res: Response) => {
-  const channel: TextChannel | undefined = client.channels.cache.find(
-    (channel) => channel.id === (process.env.CHANNEL_ID as string)
-  ) as TextChannel | undefined;
+app.get(
+  `/${process.env.SECRET}/:message`,
+  async (req: Request, res: Response) => {
+    const channel: TextChannel | undefined = client.channels.cache.find(
+      (channel) => channel.id === (process.env.CHANNEL_ID as string)
+    ) as TextChannel | undefined;
 
-  await channel?.send(req.params.message);
-  res.status(200);
-  res.send("sucess");
-});
+    await channel?.send(req.params.message);
+    res.status(200);
+    res.send("sucess");
+  }
+);
 
 app.listen(5000, () => console.log(`App listening on port 5000`));
