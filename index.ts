8b5ec2d48dc571988e90b63b7382e5974258cf03
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-23 11:29:48 +0100

    added message sending

--- index.ts
+++ index.ts
@@ -1,11 +1,25 @@
 import { config } from "dotenv";
+import express, { Request, Response } from "express";
 config();
-import Discord from "discord.js";
+import Discord, { Channel, TextChannel } from "discord.js";
+
+const app = express();
 
 const client: Discord.Client = new Discord.Client();
+client.login(process.env.TOKEN);
 
 client.on("ready", () => {
   console.log("Logged in");
 });
 
-client.login(process.env.TOKEN);
+app.get("/:message", async (req: Request, res: Response) => {
+  const channel: TextChannel | undefined = client.channels.cache.find(
+    (channel) => channel.id === (process.env.CHANNEL_ID as string)
+  ) as TextChannel | undefined;
+
+  channel?.send(req.params.message);
+  res.status(200);
+  res.send("sucess");
+});
+
+app.listen(5000, () => console.log(`App listening on port 5000`));
